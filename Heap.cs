29892ef08387038cbbf8bff468d63c73e0dcbd47
handemeseli <handemeseli1616@gmail.com>
2020-03-01 19:18:45 +0200

    examples

---
+++ Heap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalysisOfAlgorithms
+{
+    public class Heap
+    {
+        private static MinHeap container;
+
+        public static object[] Sort(object[] elements) {
+            createHeap(elements);
+            object[] sortedArray = new object[elements.Length];
+            
+            for (int i = 0; i < elements.Length; i++) {
+                sortedArray[i] = container.deleteMin();
+            }
+            return sortedArray;
+        }
+
+        private static void createHeap(object[] elements)
+        {
+            container = new MinHeap((IComparable[])elements);
+        }
+    }
+}
